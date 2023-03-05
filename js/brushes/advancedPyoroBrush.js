07f6da67595079e4d5fa7f413f7170f2935e1735
Ben Paulsen <bjhpaulsen@gmail.com>
2023-03-05 15:27:53 -0800

    speed up advancedPyoroBrush

--- js/brushes/advancedPyoroBrush.js
+++ js/brushes/advancedPyoroBrush.js
@@ -3,24 +3,24 @@ class AdvancedPyoroBrush extends Brush {
     constructor() {
         super();
         this.pyoro = loadModel('assets/models/pyoro.obj');
-        this.callsTillSpray = 0;   
+        this.callsTillSpray = 20;   
     }
 
     draw() {
         
-        if(this.callsTillSpray >= 30){
+        if(this.callsTillSpray >= 20){
             push();
-        translate(mouseX, mouseY, 0);
-        scale(brushSize/100);
-        noStroke();
+            translate(mouseX, mouseY, 0);
+            scale(brushSize/100);
+            noStroke();
 
-        rotateX(random(-PI/4, PI/4));
-        rotateY(random(-PI/4, PI/4));
-        rotateZ(random(-PI/4, PI/4));
+            rotateX(random(-PI/4, PI/4));
+            rotateY(random(-PI/4, PI/4));
+            rotateZ(random(-PI/4, PI/4));
 
-        fill(random(0,255),random(0,255),random(0,255));
-        this.model = model(this.pyoro);
-        pop();
+            fill(random(0,255),random(0,255),random(0,255));
+            this.model = model(this.pyoro);
+            pop();
             this.callsTillSpray = 0;
         }
 
